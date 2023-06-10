ef0e952972d0de4582d84af18332a218097fd25d
Null <126716874+rooting-4401@users.noreply.github.com>
2023-06-10 02:52:23 -0700

    Add files via upload

---
+++ Banner/bnr.py
@@ -0,0 +1,15 @@
+from colorama import Fore,init
+init()
+def bnr():
+    print(Fore.GREEN+'''
+ ,gggggggggggg,    ,gggggggggggg,      _,gggggg,_           ,gg,        ,ggggggggggggggg      ,a8a,  ,ggg,        gg 
+dP"""88""""""Y8b, dP"""88""""""Y8b,  ,d8P""d8P"Y8b,        i8""8i      dP""""""88"""""""     ,8" "8,dP""Y8b       dP 
+Yb,  88       `8b,Yb,  88       `8b,,d8'   Y8   "8b,dP     `8,,8'      Yb,_    88            d8   8bYb, `88      d8' 
+ `"  88        `8b `"  88        `8bd8'    `Ybaaad88P'      `88'        `""    88            88   88 `"  88    ,dP'  
+     88         Y8     88         Y88P       `""""Y8        dP"8,              88            88   88     88aaad8"    
+     88         d8     88         d88b            d8       dP' `8a             88            Y8   8P     88""""Yb,   
+     88        ,8P     88        ,8PY8,          ,8P      dP'   `Yb            88            `8, ,8'     88     "8b  
+     88       ,8P'     88       ,8P'`Y8,        ,8P'  _ ,dP'     I8      gg,   88       8888  "8,8"      88      `8i 
+     88______,dP'      88______,dP'  `Y8b,,__,,d8P'   "888,,____,dP       "Yb,,8P       `8b,  ,d8b,      88       Yb,
+    888888888P"       888888888P"      `"Y8888P"'     a8P"Y88888P"          "Y8P'         "Y88P" "Y8     88        Y8
+''')
